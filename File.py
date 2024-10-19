1c364a900280da1a135afbffa7767185755f3af4
Michal Klopotowski <123220580+MichalKlop@users.noreply.github.com>
2024-10-19 13:28:28 -0700

    Dataframe

--- File.py
+++ File.py
@@ -1,1 +1,18 @@
-#
+import pandas as pd
+
+# Courses, Names, Credits, Prerequisites
+# Assumes placement past MATH 112Z
+cs_courses = {
+    "Course Code": ["CS 210", "CS 211", "CS 212", "MATH 231", "MATH 232", "MATH 251"],
+    "Course Name": ["Computer Science I", "Computer Science II", "Computer Science III",
+                    "Elements of Discrete Mathematics I", "Elements of Discrete Mathematics II", "Calculus I"],
+    "Credits": [4, 4, 4, 4, 4, 4],
+    "Prerequisites": ["None", "CS 210", "CS 211", "MATH 251", "MATH 231", "None"]
+}
+
+
+# Create a DataFrame
+courses_df = pd.DataFrame(courses)
+
+# Display the DataFrame
+print(courses_df)
